3a16a671189c231bcb07948446d90a921d844e0d
Wan Qing <wanqing93@gmail.com>
2022-05-21 13:07:52 +0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import {Navbar} from './components'
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  return <main>
+    <Navbar/>
+    <p>Redux Toolkit</p>
+
+  </main>;
 }
 export default App;
